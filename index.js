fae1a4e0135536671cc373bf4f67525c0272b680
tamimMuntakimm <tmmuntakim123@gmail.com>
2025-06-15 16:07:45 +0600

    mongodb connection  setup

--- index.js
+++ index.js
@@ -3,12 +3,33 @@ const express = require('express');
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_password}@cluster0.qr0ehyp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 app.use(cors());
 app.use(express.json());
 
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+    }
+}
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
-    res.send('MaraThrone Server is jogging!!')
+    res.send('MaraThrone Server is jogging')
 })
 
 app.listen(port, () => {
